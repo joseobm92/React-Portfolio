9483dbddb52222efc8884c9993da3a6de49dde77
joseobm92 <joseobm92@gmail.com>
2022-12-05 18:14:58 -0500

    initial commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+import './App.css';
+
+import Nav from './components/Nav';
+import Portfolio from './components/pages/Portfolio/index';
+import Contact from './components/pages/Contact/index';
+import About from './components/pages/About/index';
+
+function App() {
+
+  const [currentPage, setCurrentPage] = useState('about');
+
+  const renderPage = () => {
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    return <About />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+
+    <body>
+      {/* // TODO: Add a comment describing what we are passing as props */}
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* // TODO: Add a comment explaining what is happening on the following line */}
+      {renderPage()}
+    </body>
+    // <div className="App">
+    //   <Nav
+
+    //     showAbout={() => setPageIndex(0)}
+    //     showPortfolio={() => setPageIndex(1)}
+    //     showContact={() => setPageIndex(2)}
+    //   />
+    //   { pageIndex === 0 ? (
+    //     <About/>
+    // ) : pageIndex === 1 ? (
+    //   <Portfolio/>
+    // ) : pageIndex === 2 ? (
+    //   <Contact/>
+    // ) : (
+    //   <About/>
+    // )}
+    // </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Nav/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Nav = ({ currentPage, handlePageChange }) => {
+    return (
+
+        // <div style={{display:'flex', justifyContent: 'center', alignItems: 'center'}}>
+        //     <h1 onClick={showAbout}>About</h1>
+        //     <h1 onClick={showPortfolio}>About</h1>
+        //     <h1 onClick={showContact}>About</h1>
+        // </div>
+
+
+        <nav className="navbar navbar-expand-lg bg-dark navbar-dark py-3 mb-4">
+            <div className="container">
+                
+                <a href="#about" className="navbar-brand" onClick={() => handlePageChange('About')} >Jose Barreto</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu"><span
+                    className="navbar-toggler-icon"></span></button>
+
+                <div className="collapse navbar-collapse" id="navmenu">
+                    <ul className="navbar-nav ms-auto ml-3">
+                        <li className="nav-item">
+                            <a href="#about" className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'} onClick={() => handlePageChange('About')}> About </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#contact" className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'} onClick={() => handlePageChange('Contact')}> Contact </a>
+                        </li>
+                        <li className="nav-item">
+                            
+                            <a href="#portfolio" className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'} onClick={() => handlePageChange('Portfolio')} > Portfolio  </a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    );
+};
+
+export default Nav;
