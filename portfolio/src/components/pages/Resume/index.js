11f697c3f7daddb93ca4c4bb42680a1010872da2
joseobm92 <joseobm92@gmail.com>
2022-12-07 17:07:25 -0500

    minor additions

--- portfolio/src/components/pages/Resume/index.js
+++ portfolio/src/components/pages/Resume/index.js
@@ -2,23 +2,28 @@ import React from 'react';
 
 import '../Resume/resume.css'
 
+import resume from '../../directory/resume.pdf'
+
 export default function Contact() {
     return (
         <div className='container'>
             <div className='row'>
                 <h1> Resume </h1>
+                <a className='text-decoration-none link-dark' href={resume} download ='resume'>Download the Resume <i className="bi bi-file-earmark-person"></i> </a>
                 <div className='col-md-5 mx-3 my-3 front shadow'>
 
                     <h2 className='px-2 py-2'>Front-end Proficiencies</h2>
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item">HTML</li>
-                        <li class="list-group-item">CSS</li>
-                        <li class="list-group-item">JavaScript</li>
+                        <li class="list-group-item">HTML5</li>
+                        <li class="list-group-item">CSS3</li>
+                        <li class="list-group-item">JavaScript ES6+</li>
                         <li class="list-group-item">Jquery</li>
+                        <li class="list-group-item">Jquery UI</li>
                         <li class="list-group-item">Responsive Design</li>
                         <li class="list-group-item">React</li>
                         <li class="list-group-item">BootStrap</li>
                         <li class="list-group-item">Bulma</li>
+                        <li class="list-group-item">Google Fonts</li>
                     </ul>
                 </div>
                 <div className='col-md-5 mx-3 my-3 back shadow'>
