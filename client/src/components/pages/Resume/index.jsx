ca9c6a77c9897d6cac32874cc8928bfe7531df60
joseobm92 <joseobm92@gmail.com>
2022-12-19 16:52:56 -0500

    fixed resume

--- client/src/components/pages/Resume/index.jsx
+++ client/src/components/pages/Resume/index.jsx
@@ -11,7 +11,7 @@ export default function Contact() {
                 <h1> Resume </h1>
                 <a className='text-decoration-none link-dark' href={resume} download='resume'>Download the Resume <i className="bi bi-file-earmark-person"></i> </a>
                 
-                    <div className='col-sm-5 my-3 front shadow'>
+                    <div className='col-md-5 m-2 front shadow'>
                         <h2 className='px-2 py-2'>Front-end Proficiencies</h2>
                         <ul class="list-group list-group-flush">
                             <li class="list-group-item">HTML5</li>
@@ -25,12 +25,11 @@ export default function Contact() {
                             <li class="list-group-item">Bulma</li>
                             <li class="list-group-item">Google Fonts</li>
                         </ul>
-
                     </div>
                 
                 
 
-                <div className='col-sm-5  my-3 back shadow'>
+                <div className='col-md-5  m-2 back shadow'>
 
                     <h2 className='px-2 py-2'>Back-end Proficiencies</h2>
                     <ul class="list-group list-group-flush">
